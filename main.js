d1014f1e78a0de17a7e577809e82601df218f24f
Kastor Nevierre <no-reply@kastor-ffxiv.com>
2020-10-16 22:32:36 +0100

    initial

--- main.js
+++ main.js
@@ -5,15 +5,17 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1600,
+    height: 1080,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
+  mainWindow.setMenu(null);
+
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('https://discord.com')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
